e033ca13acbd7cd125154e3d5350fa22afbeac6d
kevinyugi <102214624+Kevinyugi001@users.noreply.github.com>
2022-04-19 12:23:32 +0300

    Add files via upload

---
+++ Intro.php
@@ -0,0 +1,14 @@
+<?php
+echo "This is my first php code";
+echo "<br>";
+echo "How is today's class";
+echo "<br>";
+echo 23 + 56;
+echo "<br>";
+echo 55 - 20;
+echo "<br>";
+echo 25 / 5;
+echo "<br>";
+echo 5 * 5;
+    ?>
+
